9633006378d0761d7abab9ee189d7a5a8a82a15b
rudomachsi <rudomachisi@gmail.com>
2024-08-11 12:28:09 +0200

    updated code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function refreshWeather(response) {
+
+  function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
