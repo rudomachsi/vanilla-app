99c5bf89a34cfb341fff52e87f586cc00609bb77
rudomachsi <rudomachisi@gmail.com>
2024-08-11 12:32:49 +0200

    api code updated

--- index.js
+++ index.js
@@ -41,7 +41,7 @@
   }
   
   function searchCity(city) {
-    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiKey = "b4tba9bff663492fbddb13d5a4800eo6";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(refreshWeather);
   }
